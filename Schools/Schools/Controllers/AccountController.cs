55d0ab73f2a3b0a19d655d12397c3052f4086fa4
Ivan <ivan.sstoyanov@abv.bg>
2021-01-17 22:19:38 +0200

    DataAnnotation added

--- Schools/Schools/Controllers/AccountController.cs
+++ Schools/Schools/Controllers/AccountController.cs
@@ -24,6 +24,7 @@ namespace Schools.Controllers
       return View(nameof(Login));
     }
 
+    [HttpPost]
     public async Task<IActionResult> Login(LoginModel model)
     {
       if (!ModelState.IsValid)
